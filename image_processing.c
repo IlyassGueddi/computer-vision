85bcf3dc825c44e00864745556ba23d9439cf847
IlyassGueddi <gueddiilyass99@gmail.com>
2026-03-12 11:51:58 +0000

    sharpness

--- image_processing.c
+++ image_processing.c
@@ -21,12 +21,10 @@ int main() {
     //read the pixels into array
     fread(data, sizeof(unsigned char), size , img);
 
-    int choice, brightness,v;
+    int choice, brightness,v,index;
     unsigned char *temp_data = (unsigned char *)malloc(size);
-    int sharp_ker = [0,-v,0,
-                    -v,1+4*v,-v,
-                    0,-v,0];
-
+    
+    int sharp_ker[3][3] = {{0,-v,0},{-v,(1+4*v),-v},{0,-v,0}};
     while(true){
 
         /*
@@ -86,8 +84,59 @@ int main() {
         }else if(choice == 2){
             printf("sharpeness value: ");
             scanf("%d", &v);
+           
+            for (int y = 1; y < height; y++) {
+                for (int x = 1; x < width; x++) {
+                    // pixel (x, y)
+                    // every pixel is 3 values R,G,B
+                    for (int c = 0; c < 3; c++) {
+                        
+                        int index = (y * width + x) * 3 + c;
+                        
+                        float sum = 0.0;
+
+                        for (int i = 0; i < 3; i++) {      // kernel Row 
+                            for (int j = 0; j < 3; j++) {  // Kernel Column 
+
+                                int ky = i - 1; 
+                                int kx = j - 1;
+
+                                int neighbor_idx = ((y + ky) * width + (x + kx)) * 3 + c;
+
+                                // Multiply by kernel weight
+                                sum += (float)data[neighbor_idx] * sharp_ker[i][j];
+                            }
+                        }
+                        if (sum > 255) sum = 255;
+                        if (sum < 0) sum = 0;
+
+                        temp_data[index] = (unsigned char)sum;
+                    }
+                }
+            }
+            //Open a new file for Writing in Binary mode 
+            FILE *out = fopen("output.ppm", "wb");
+
+            if (out == NULL) {
+                perror("Could not create output file");
+                return 1;
+            }
+
+            // Write the PPM Header
+            // P6 = Binary RGB, then Width, Height, and Max Color Value
+            fprintf(out, "P6\n%d %d\n255\n", width, height);
+
+            // Write the Array
+            // '1' is the size of each element (char), 'size' is the total bytes
+            fwrite(temp_data, 1, size, out);
+
+            // Close the file to save it to disk
+            fclose(out);
+
+            printf("Image saved successfully as output.ppm\n");
+
         }else if(choice == 3){
-            
+            break;
         }else{
             printf("invalid value;\n");
             break;
@@ -97,5 +146,6 @@ int main() {
 
 
     free(data);
+    free(temp_data);
     fclose(img);
 }
